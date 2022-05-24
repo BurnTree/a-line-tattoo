40f473628ec35d11a0c994390d69144463c38aa4
Ilya Yeliseyeu <Ilya.Yeliseyeu@ibm.com>
2022-05-24 13:28:33 +0300

    fix contact rows

--- src/components/Head/HeadContact.js
+++ src/components/Head/HeadContact.js
@@ -5,13 +5,10 @@ import {Phone, SocialNetwork} from "../ContactLinks";
 const HeadContact = () => {
     return (
         <Row >
-            <Col className="center-container" sm={24} md={12} lg={12}>
+            <Col className="center-container" xs={24} sm={24} md={24} lg={12}>
                 <Phone/>
             </Col>
-            <br/>
-            <br/>
-            <br/>
-            <Col className="center-container" sm={24} md={12} lg={12}>
+            <Col className="center-container" xs={24} sm={24} md={24} lg={12}>
                 <SocialNetwork/>
             </Col>
         </Row>
